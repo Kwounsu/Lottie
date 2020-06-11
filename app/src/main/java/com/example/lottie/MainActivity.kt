245c7beccfe0cbe7b337714446c698a6973a3c9f
Kwounsu <sb00lgs@gmail.com>
2020-06-11 13:44:43 -0700

    Lottie Animation Implemented

--- app/src/main/java/com/example/lottie/MainActivity.kt
+++ app/src/main/java/com/example/lottie/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.lottie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        av_from_code.setAnimation("online_school.json")
+        av_from_code.playAnimation()
+        av_from_code.loop(true)
     }
 }
